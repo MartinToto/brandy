94bbf6bdc32738bae376bab11a2852cddb35ba96
martin <mandalahua@163.com>
2019-12-24 22:09:14 +0800

    first commit

---
+++ src/main/java/com/martin/brandy/BrandyApplication.java
@@ -0,0 +1,13 @@
+package com.martin.brandy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrandyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/martin/brandy/BrandyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.brandy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
